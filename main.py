b573c23dabbb3e88884efcee2e534edc795667a3
Diogo Lima <diogooo@gmail.com>
2019-10-25 13:47:47 -0300

    initial

---
+++ main.py
@@ -0,0 +1,91 @@
+from typing import List
+from pygments import highlight
+from pygments.lexers import guess_lexer
+from pygments.formatters import HtmlFormatter
+from fastapi import FastAPI, HTTPException
+from starlette.status import (HTTP_201_CREATED, HTTP_400_BAD_REQUEST,
+                              HTTP_404_NOT_FOUND)
+from starlette.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
+from models import (PasteRequestModel,
+                    PasteResponseModel,
+                    Paste)
+
+app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def get_paste(id: int) -> Paste:
+    try:
+        paste: Paste = Paste.get(Paste.id == id)
+        return paste
+    except Paste.DoesNotExist:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Paste not found.")
+
+
+def create_paste(paste: PasteRequestModel) -> Paste:
+    if not paste.title or not paste.text:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Bad request.")
+    paste_obj = Paste.create(
+        title=paste.title,
+        text=paste.text,
+        signature=paste.signature
+    )
+    return paste_obj
+
+
+@app.get("/{id}")
+async def index(id: int) -> PasteResponseModel:
+    paste = get_paste(id)
+    text = highlight(paste.text, guess_lexer(paste.text), HtmlFormatter())
+    paste_data = {
+        "id": paste.id,
+        "title": paste.title,
+        "text": text,
+        "created_at": paste.created_at,
+        "signature": paste.signature
+    }
+
+    return PasteResponseModel(**paste_data)
+
+
+@app.get("/{id}/related")
+async def index(id: int) -> List[PasteResponseModel]:
+    paste = get_paste(id)
+    pastes: List[PasteResponseModel]
+
+    if paste.signature:
+        entries = Paste.select().where(
+            Paste.signature == paste.signature,
+            Paste.id != paste.id
+        ).order_by(Paste.created_at.desc()).dicts()
+        pastes = [
+            PasteResponseModel(**paste) for
+            paste in entries
+        ]
+        return pastes
+
+    return []
+
+
+@app.post("/", status_code=HTTP_201_CREATED)
+async def create(paste: PasteRequestModel) -> PasteResponseModel:
+    paste_obj = create_paste(paste)
+    return PasteResponseModel(
+        id=paste_obj.id,
+        title=paste_obj.title,
+        text=paste_obj.text,
+        created_at=paste_obj.created_at,
+        signature=paste_obj.signature
+    )
---
+++ models.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from environs import Env
+import pydantic
+import peewee
+
+
+env = Env()
+env.read_env()
+database = env.str("DATABASE")
+db = peewee.SqliteDatabase(database)
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+
+class Paste(BaseModel):
+    title = peewee.CharField()
+    text = peewee.TextField()
+    created_at = peewee.DateTimeField(default=datetime.now)
+    signature = peewee.TextField(null=True)
+
+
+class PasteRequestModel(pydantic.BaseModel):
+    title: str
+    text: str
+    signature: str = None
+
+
+class PasteResponseModel(pydantic.BaseModel):
+    id: int
+    title: str
+    text: str
+    created_at: datetime
+    signature: str = None
+
+
+def create_tables():
+    db.connect()
+    db.create_tables([Paste])
+
+if __name__ == '__main__':
+    create_tables()
